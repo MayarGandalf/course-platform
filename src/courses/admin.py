da6ef3f3f50dfe32b42d3d701f2b32185a0495a2
igor <kanyewestlover9124@gmail.com>
2025-01-19 23:57:52 +0300

    Оптимизация функций Cloudinary

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -2,16 +2,23 @@ from django.contrib import admin
 from django.utils.html import format_html
 from .models import Course,  Lesson
 from cloudinary import CloudinaryImage
-
+import helpers
 
 class LessonInline(admin.StackedInline ):
     model = Lesson 
     readonly_fields = [
         'public_id', 
-        'updated', 
+        'updated',
+        'display_image' 
     ]
     extra = 0
 
+    def display_image(self, obj, *args, **kwargs): 
+        url = helpers.get_cloudinary_image_object(obj,field_name = 'thumbnail', width=200)
+        return format_html(f"<img src = {url} />")
+    
+    display_image.short_description = "Current Image"
+    
 
 
 @admin.register(Course)
@@ -23,7 +30,7 @@ class CourseAdmin(admin.ModelAdmin):
     readonly_fields = ['public_id','display_image']
 
     def display_image(self, obj, *args, **kwargs): 
-        url = obj.image_admin_url
+        url = helpers.get_cloudinary_image_object(obj,field_name = 'image', width=200)
         return format_html(f"<img src = {url} />")
     
     display_image.short_description = "Current Image"
--- src/courses/models.py
+++ src/courses/models.py
@@ -111,27 +111,6 @@ class Course(models.Model):
     def is_published(self): 
         return self.status == PublishStatus.PUBLISHED
     
-    @property
-    def image_admin_url(self): 
-        if not self.image:
-            return ""
-        image_options = { 
-            "width": 200
-        }
-        url = self.image.build_url(**image_options)
-        return url 
-    
-    def get_image_thumbnail(self, as_html = False, width = 200 ): 
-        if not self.image:
-            return ""
-        image_options = { 
-            "width": width
-        }
-        if as_html: 
-            return self.image.image(**image_options)
-        url = self.image.build_url(**image_options)
-        return url
-    
 
 class Lesson(models.Model): 
     course = models.ForeignKey(Course, on_delete = models.CASCADE)
