b9eb2dd03a8fa0b14ab004b34f4112ccc28e8942
igor <kanyewestlover9124@gmail.com>
2025-01-20 13:10:23 +0300

    JS для плеера видео. Исправление мелких багов

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -20,9 +20,14 @@ class LessonInline(admin.StackedInline ):
     
     display_image.short_description = "Current Image"
 
-    def display_video(self, obj, *args, **kwargs): 
-        url = helpers.get_cloudinary_video_object(obj,field_name = 'video', width=550, as_html = True)
-        return format_html(f"{url}")
+    def display_video(self, obj, *args, **kwargs):
+        video_embed_html = helpers.get_cloudinary_video_object(
+            obj, 
+            field_name='video',
+            as_html=True,
+            width=550
+        )
+        return video_embed_html
     
     display_video.short_description = "Current Video"    
     
@@ -31,19 +36,20 @@ class LessonInline(admin.StackedInline ):
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     inlines = [LessonInline]
-    list_display = ['title', 'status', 'access',]
+    list_display = ['title', 'status', 'access']
     list_filter = ['status', 'access']
-    fields = ['public_id','title', 'description', 'status', 'image', 'access', 'display_image']
-    readonly_fields = ['public_id','display_image']
+    fields = ['public_id', 'title', 'description', 'status', 'image', 'access', 'display_image']
+    readonly_fields = ['public_id', 'display_image']
 
-    def display_image(self, obj, *args, **kwargs): 
-        url = helpers.get_cloudinary_image_object(obj,field_name = 'image', width=200)
-        return format_html(f"<img src = {url} />")
-    
+    def display_image(self, obj, *args, **kwargs):
+        url = helpers.get_cloudinary_image_object(
+            obj, 
+            field_name='image',
+            width=200
+        )
+        return format_html(f"<img src={url} />")
+
     display_image.short_description = "Current Image"
-
-
-
 
 
 
