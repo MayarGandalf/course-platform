6ac1dff43730bc63ae76120cc9179c6dfdfb57b4
igor <kanyewestlover9124@gmail.com>
2025-01-23 18:40:37 +0300

    Функции для верификации

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from emails.forms import EmailForm 
 from django.conf import settings
 from emails.models import Email, EmailVerificationEvent
-
+from emails import services as emails_services
 
 EMAIL_ADDRESS = settings.EMAIL_ADDRESS
 
@@ -15,10 +15,10 @@ def home_view(request, *args, **kwargs):
     }
     if form.is_valid():
         email_val = form.cleaned_data.get('email')
-        email_obj, created = Email.objects.get_or_create(email=email_val)
-        obj = EmailVerificationEvent.objects.create(parent = email_obj, email = email_val)
-        # obj = form.save()
-        # print(obj)
+        obj = emails_services.start_verification_event(email_val)
+        print(obj)
         context['form'] = EmailForm()
         context['message'] = f"Success! Check email for verification from {EMAIL_ADDRESS}"
+    else: 
+        print(form.errors)
     return render(request, template_name, context)
---
+++ src/emails/services.py
@@ -0,0 +1,11 @@
+from .models import Email, EmailVerificationEvent
+
+def verify_email(email):
+    qs = Email.objects.filter(email = email, active = False)
+    return qs.exists()
+    pass
+
+def start_verification_event(email): 
+    email_obj, created = Email.objects.get_or_create(email=email)
+    obj = EmailVerificationEvent.objects.create(parent = email_obj, email = email)
+    return obj
